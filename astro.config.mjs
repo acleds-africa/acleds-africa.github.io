8a44ce48ac69cb956813c908b27db76674c9df1f
prablordeppey <prablordeppey@gmail.com>
2026-06-21 06:19:22 +0000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
+
+  integrations: [mdx()],
+});
